104b9a1a447629f6e28ef7a77940b5459b09d8e8
TMBeaver <mantasbaltramaitis1@gmail.com>
2023-09-18 10:33:58 +0300

    new button, text view

--- app/src/main/java/com/example/antropirmas/MainActivity.java
+++ app/src/main/java/com/example/antropirmas/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView TVMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.TVMain = findViewById(R.id.TVMAin);
+
     }
 
     public void OnButtonClickChangeText (View view) {
-
+this.TVMain.setText("Changed text");
 
     }
 }
