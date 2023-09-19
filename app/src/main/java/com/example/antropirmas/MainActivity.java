77fac0371793ce99dd6debf5569e6525dab2b40f
TMBeaver <mantasbaltramaitis1@gmail.com>
2023-09-19 16:37:33 +0300

    new button, text view

--- app/src/main/java/com/example/antropirmas/MainActivity.java
+++ app/src/main/java/com/example/antropirmas/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.core.content.ContextCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 TVMain.setText("Changed text");
 
+                int color = ContextCompat.getColor(MainActivity.this, android.R.color.holo_red_dark);
+                TVMain.setTextColor(color);
 
-        ButtOff1.setOnClickListener(new View.OnClickListener() {
+                ButtOff1.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View view) {
